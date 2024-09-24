20792f5b758ba226bf120dc277816b648a03398b
Adithyan T <adithsreedhar628@gmail.com>
2024-09-25 03:30:52 +0530

    added map

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const http = require("http");
+const path = require("path");
+
+const app = express();
+
+const socketio = require("socket.io");
+const server = http.createServer(app);
+const io = socketio(server);
+
+io.on("connection", (socket) => {
+  console.log("New WebSocket connection");
+});
+
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+server.listen(3000);
